667e4413281b33ccb9e8c0e418b13c97b1afbeec
Gerrit Pape <papeg@mail.upb.de>
2025-04-22 16:43:01 +0200

    plot results

---
+++ plot.jl
@@ -0,0 +1,21 @@
+using DataFrames
+using CSV
+using CairoMakie
+
+df = CSV.read("results.csv", DataFrame)
+
+fig = Figure()
+
+xticks = 2 .^ range(5, 16)
+
+ax = Axis(fig[1, 1], xlabel="Cores", ylabel="MFLOPs", xscale=log2, xticks = (xticks, string.(xticks)), ytickformat = "{:.3f}")
+
+data = []
+
+for df_group in groupby(df, "Communication")
+    push!(data, lines!(ax, df_group[!, "Ranks"], df_group[!, "MFlops_per_s"], label="$(first(df_group[!, "Communication"]))"))
+end
+
+Legend(fig[1, 2], [d for d in data], [d.label for d in data])
+
+save("plot.pdf", fig)
