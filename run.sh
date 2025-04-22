c7fb7a35ebe977d4f867bce9cb9a2a206ddd4695
Gerrit Pape <papeg@mail.upb.de>
2025-04-22 16:08:12 +0200

    update runscript

--- run.sh
+++ run.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
-#SBATCH -t 00:30:00
+#SBATCH -t 00:10:00
 #SBATCH -J "Synch_p2p"
 #SBATCH -p normal
+#SBATCH --exclusive
 
 source env.sh
 
 ranks=$SLURM_NTASKS
 
-threads_per_rank=32
-ranks_omp=$(($ranks / $threads_per_rank))
+max_threads_per_rank=128
 
-iterations=1000
-m=1280
-n=12800
+if (( ranks < max_threads_per_rank )); then
+    ranks_omp=1
+    threads_per_rank=$ranks
+else
+    ranks_omp=$(( ranks / max_threads_per_rank ))
+    threads_per_rank=$max_threads_per_rank
+fi
+
+iterations=1
+m=65536
+n=65536
 
 echo "running MPI1"
-srun -o mpi1_$ranks-%j.out -e mpi1_$ranks.out -n $ranks ./PRK/MPI1/Synch_p2p/p2p $iterations $m $n
+srun -o mpi1_${ranks}-%j.out -e mpi1_${ranks}-%j.out -n $ranks ./PRK/MPI1/Synch_p2p/p2p $iterations $m $n
 
 echo "running MPIRMA"
-srun -o mpirma_$ranks-%j.out -e mpirma_$ranks.out -n $ranks ./PRK/MPIRMA/Synch_p2p/p2p $iterations $m $n
+srun -o mpirma_${ranks}-%j.out -e mpirma_${ranks}-%j.out -n $ranks ./PRK/MPIRMA/Synch_p2p/p2p $iterations $m $n
 
 echo "running MPISHM"
-srun -o mpishm_$ranks-%j.out -e mpishm_$ranks.out -n $ranks ./PRK/MPISHM/Synch_p2p/p2p $iterations $m $n
+srun -o mpishm_${ranks}-%j.out -e mpishm_${ranks}-%j.out -n $ranks ./PRK/MPISHM/Synch_p2p/p2p $iterations $m $n
 
 echo "running MPIOMP"
-srun -o mpiomp_$ranks-%j.out -e mpiomp_$ranks.out -n $ranks_omp ./PRK/MPIOPENMP/Synch_p2p/p2p $threads_per_rank $iterations $m $n
+srun --spread-job --cpus-per-task=${threads_per_rank} -o mpiomp_${ranks}-%j.out -e mpiomp_${ranks}-%j.out -n $ranks_omp ./PRK/MPIOPENMP/Synch_p2p/p2p $threads_per_rank $iterations $m $n
+
+echo "done"
