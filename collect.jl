4a1981de7c2604630d6db6bae77f11b0c9c11103
Gerrit Pape <papeg@mail.upb.de>
2025-05-08 13:38:01 +0200

    update collect script

--- collect.jl
+++ collect.jl
@@ -1,14 +1,15 @@
 using DataFrames
 using CSV
 
-function find_latest(comm, num_ranks)
+function find_latest(to_find)
     latest_file = ""
     latest_job_id = 0
     for file in readdir(".")
         if isfile(file)
-            if occursin(comm, file) && occursin("_" * string(num_ranks) * "-", file) && occursin(".out", file) 
-                for match in eachmatch(r"\d+", file)
-                    job_id = parse(Int, match.match)
+            if occursin(to_find, file) && occursin(".out", file) 
+                for match in eachmatch(r"(\d+)\.out$", file)
+                    display(match.match)
+                    job_id = parse(Int, split(match.match, ".")[1])
                     if job_id > latest_job_id
                         latest_file = file
                         latest_job_id = job_id
@@ -21,23 +22,31 @@ function find_latest(comm, num_ranks)
 end
 
 df = DataFrame(Communication = String[], Ranks = Int[], MFlops_per_s = Float64[], Validates = Bool[])
-for comm in ["mpi1", "mpiomp", "mpirma", "mpishm"]
-    for n in 2 .^ range(5, 16)
-        file = find_latest(comm, n)
-        if file == ""
-            println(n, " ", comm, " not found")
-            continue
+for comm in [["mpi1", "mpirma", "mpishm"];["mpiomp_" * string(Int(i)) for i in logrange(1, 128, 2)]]
+    for problem_size in [49152]
+        for n in 2 .^ range(5, 16)
+            to_find = join([comm, n, problem_size, problem_size], "_")
+            file = find_latest(to_find)
+            if file == ""
+                println(" ", to_find, " not found")
+                continue
+            end
+            output = read(file, String)
+            validates = occursin("Solution validates", output)
+            splitted = split(output)
+            found = findfirst(v -> v == "(MFlops/s):", splitted)
+            mflops_per_s = if found == nothing
+                0.0
+            else
+                parse(Float64, splitted[found + 1])
+            end
+            push!(df,(
+                Communication = comm,
+                Ranks = n,
+                MFlops_per_s = mflops_per_s,
+                Validates = validates,
+           ))
         end
-        output = read(file, String)
-        validates = occursin("Solution validates", output)
-        splitted = split(output)
-        mflops_per_s = parse(Float64, splitted[findfirst(v -> v == "(MFlops/s):", splitted) + 1])
-        push!(df,(
-            Communication = comm,
-            Ranks = n,
-            MFlops_per_s = mflops_per_s,
-            Validates = validates,
-       ))
     end
 end
 
